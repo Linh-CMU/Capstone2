b30f6f15a9ff004f072ebf02c23fa785dda59bcc
Linh-CMU <linhnguyenyhqh@gmail.com>
2022-05-25 23:36:14 +0700

    Done project

--- src/containers/User/UserItem/index.js
+++ src/containers/User/UserItem/index.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "@mui/material/Button";
 import "./_useritem.scss";
 
 const UserItem = (props) => {
+    const [textButton, setTextButton] = useState("")
     const { userName, status, handleOpen, setUserSelected } = props;
+    useEffect(() => {
+        let url = window.location.href;
+        var id = url.substring(url.lastIndexOf('/') + 1);
+        setTextButton(id == "payment" ? "Click to pay" : id == "booked" ? "Confirm" : "View detail")
+    }, [window.location.href])
     return (
         <div className="MentorDashboard__listItem UserItem__listItem ">
             <div className="MentorDashboard__listItem__name">{userName}</div>
-            <div style={{flex: "0 0 40%", padding: "0 10px"}}  >
+            <div style={{ flex: "0 0 40%", padding: "0 10px" }}  >
                 <Button
                     onClick={() => {
                         setUserSelected(props);
@@ -15,7 +21,7 @@ const UserItem = (props) => {
                     }}
                     variant="outlined"
                 >
-                    Click to pay
+                    {textButton}
                 </Button>
             </div>
             <div className="MentorDashboard__listItem__status">
--- src/contexts/mentorContext.js
+++ src/contexts/mentorContext.js
@@ -7,69 +7,8 @@ const MentorContext = createContext();
 
 export const MentorContextProvider = ({ children }) => {
     const [userList, setUserList] = useState([
-        // {
-        //     createBy: "phucs54123",
-        //     createDate: "2022-05-17T21:33:08",
-        //     modifiedBy: "phucs54123",
-        //     modifiedDate: "2022-05-17T21:33:08",
-        //     orderId: 8,
-        //     userOrder: null,
-        //     professionalOrder: {
-        //         createBy: "anonymousUser",
-        //         createDate: "2022-05-16T11:33:27",
-        //         modifiedBy: "anonymousUser",
-        //         modifiedDate: "2022-05-16T11:33:27",
-        //         id: 6,
-        //         nameCompany: "nameCompany",
-        //         addressCompany: "address",
-        //         phoneCompany: "0616162662",
-        //         emailCompany: "email@gmail.com",
-        //         status: "is_active",
-        //         linkAvatar: "linkAvatar",
-        //         timeStart: "10:24:00",
-        //         price: null,
-        //         userResource: {
-        //             createBy: "anonymousUser",
-        //             createDate: "2022-05-16T11:33:26",
-        //             modifiedBy: "anonymousUser",
-        //             modifiedDate: "2022-05-16T11:33:26",
-        //             userId: 8,
-        //             userName: "phucs54123",
-        //             addressOfUser: null,
-        //             firstName: "Dat",
-        //             lastName: "Nguyen",
-        //             email: "thangphu1012@gmail.com",
-        //             gender: "male",
-        //             dateOfBirth: "1999-02-03",
-        //             numberPhone: "0616162662",
-        //         },
-        //         listDayOfWeek: ["Tuesday", "Friday", "Saturday"],
-        //         specializationResource: {
-        //             createBy: "user1234",
-        //             createDate: "2022-05-15T18:32:33",
-        //             modifiedBy: "user1234",
-        //             modifiedDate: "2022-05-15T18:32:33",
-        //             id: 1,
-        //             name: "Developer Back End",
-        //             description: "Developer Back End",
-        //             status: true,
-        //         },
-        //         statusWithUser: null,
-        //     },
-        //     statusMeeting: "pending",
-        //     timeStart: "2022-05-17T14:33:01",
-        //     timeEnd: null,
-        // },
     ]);
     const [userListPending, setUserListPending] = useState([]);
-
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         await getAllUsers();
-    //     };
-    //     if (Utils.getSavedToken() && Utils.getSavedAuthority() !== "USER")
-    //         fetchData();
-    // }, []);
 
     useEffect(() => {
         setUserList(userListPending);
@@ -83,7 +22,6 @@ export const MentorContextProvider = ({ children }) => {
         await API.getListPending()
             .then(async (res) => {
                 if (res.status === 200) {
-                    // console.log(res.data.data[0].curriculumVitaeUserOrder, "test")
                     setUserListPending(
                         res?.data?.data?.map((item) => {
                             return {
